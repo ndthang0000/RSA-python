d35045c5dd276c767a4f5943dde1792bdc9b7ead
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2022-01-03 23:33:23 +0700

    improve card, create upload template

---
+++ template/assets/js/main.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded",(e)=>{
+
+    //check login
+    let isLogin=JSON.parse(localStorage.getItem('isLogin'))
+    if(!isLogin){
+        window.location.href=window.location.pathname.replace('index','login')
+    }
+    
+    //log out
+    let btnLogOut=document.querySelector('#log-out')
+    btnLogOut.addEventListener('click', (e)=>{
+        localStorage.setItem('isLogin', JSON.stringify(false))
+        window.location.href=window.location.pathname.replace('index','login')
+    })
+
+    
+    // toggle image avatar
+    const imageBtn=document.querySelector('.image-area')
+    const logoutEl=document.querySelector('.list-item')
+    document.addEventListener('click',(e)=>{
+        if(e.target.classList.value==='img-avatar'){
+            logoutEl.classList.toggle('ds-none-block')
+        }
+        else{
+            logoutEl.classList.add('ds-none-block')
+        }
+        
+    })
+
+
+    // active menu item
+    const menuEl=document.querySelectorAll('.menu-list a')
+    console.log(menuEl)
+    menuEl.forEach(item=>{
+        console.log(item.href)
+        if(item.href===location.href){
+            item.classList.add('active')
+        }
+    })
+    
+
+    // side bar menu
+    const headerEl=document.querySelector('.header-logo')
+    const sideBarEl=document.querySelector('.side-bar-menu')
+    headerEl.addEventListener('click',(e)=>{
+        sideBarEl.classList.toggle('hidden-side-bar')
+    })
+})
