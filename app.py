ed01939fd4e36fb19adeb422d04e9fbc615c0315
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2022-01-08 17:12:49 +0700

    creat image collection& jwt

--- app.py
+++ app.py
@@ -11,13 +11,16 @@ import bcrypt
 import uuid
 import os
 import datetime
+import jwt
+import json
 
 app = Flask(__name__)
-CORS(app)
+CORS(app) 
 
 UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+secretKey='keyjwt'
 # img = Image.open("static/upload/52785b27-aafc-4ae8-a5bf-f590664b4766.png")
 # print(img)
 # new_img = img.filter(ImageFilter.GaussianBlur(radius=5))
@@ -29,11 +32,30 @@ db = mongodb_client.db
 
 @app.route('/api/upload', methods=['POST'])
 def upload():
-    file_to_upload = request.files['image']
-    extension = os.path.splitext(file_to_upload.filename)[1]
-    f_name = str(uuid.uuid4()) + extension
-    file_to_upload.save(os.path.join('static/upload', f_name))
-    return jsonify({'success': True})
+    # file_to_upload = request.files['image']
+    # extension = os.path.splitext(file_to_upload.filename)[1]
+    # f_name = str(uuid.uuid4()) + extension
+    # file_to_upload.save(os.path.join('static/upload', f_name))
+    # return jsonify({'success': True})
+
+    rawImage=request.json['image']
+    width=request.json['width']
+    height=request.json['height']
+    name=request.json['name']
+    token=request.headers['Authorization'].split(' ')[1]
+    username = jwt.decode(token,secretKey, algorithms=['HS256'])
+    user = db.user.find_one({"username": username['username']})
+    if user:
+        db.image.insert_one({'ciphertext': rawImage,
+                            'user':user['_id'],
+                            'name': name,
+                            'width': width,
+                            'height':height,
+                            'createdAt': datetime.datetime.now()
+                            })
+        return jsonify(success=True)
+    else:
+        return jsonify(success=False,message='User không tồn tại')
 
 
 @app.route('/api/register', methods=['POST'])
@@ -55,16 +77,35 @@ def register():
 
 @app.route('/api/login', methods=['POST'])
 def login():
-    print('vo day')
     username = request.json['username']
     password = request.json['password']
-    print(username)
+    encoded_jwt = jwt.encode({'username': username},secretKey, algorithm='HS256')
     user = db.user.find_one({"username": username})
     if user:
         if bcrypt.checkpw(password.encode(), user['password']):
-            return jsonify(success=True)
+            return jsonify(success=True,token=encoded_jwt)
         return jsonify(success=False, message='Mật khẩu không chính xác')
     return jsonify(success=False, message='Tài khoản không tồn tại')
+
+@app.route('/api/my-image', methods=['GET'])
+def get_image():
+    token=request.headers['Authorization'].split(' ')[1]
+    print(token)
+    username = jwt.decode(token,secretKey, algorithms=['HS256'])
+    print(username)
+    user = db.user.find_one({"username": username['username']})
+    print(user)
+    if user:
+        all_image=db.image.find_one({"user":user['_id']})
+        print(user['_id'])
+        print(all_image)
+        return json.dump(success=True,all_image=all_image)
+        # return jsonify(
+        #         success=True, 
+        #         all_image=dumps(all_image)
+        # )
+    else:
+        return jsonify(success=False,message='User không tồn tại')
 
 @app.route('/api/test', methods=['GET'])
 def test():
